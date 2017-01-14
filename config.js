26ce9247c3a243dfc35b5a23cd0a0a3c1c866642
eslamNasser <eslamnasser@hmaserv.net>
2017-01-14 19:28:52 +0200

    Chenge heroku config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     'secret': 'WweRrMh6hq5dFy2QjTXs',
-    //'database': 'mongodb://root:root@ds151018.mlab.com:51018/mvc_blog' // Online DB BLOG
-    'database': 'mongodb://localhost:27017/blog'
+    'database': 'mongodb://root:root@ds151018.mlab.com:51018/mvc_blog' // Online DB BLOG
+    // 'database': 'mongodb://localhost:27017/blog'
     
 };
