a1fd09dabc75e275a16f4983f3dc22bb6456a2f0
eslamNasser <eslamnasser@hmaserv.net>
2017-01-24 19:51:33 +0200

    Major commit #2

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     'secret': 'WweRrMh6hq5dFy2QjTXs',
-    'database': 'mongodb://root:WweRrMh6hq5dFy2QjTXs@jello.modulusmongo.net:27017/A4voheje' // Online DB BLOG
-    // 'database': 'mongodb://localhost:27017/blog'
+    // 'database': 'mongodb://root:WweRrMh6hq5dFy2QjTXs@jello.modulusmongo.net:27017/A4voheje' // Online DB BLOG
+    'database': 'mongodb://localhost:27017/blog'
     
 };
